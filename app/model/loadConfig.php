76217f5f6b09b42505ecd14610381057ab9c9ab0
zuoshoupai <15373850656@163.com>
2020-01-02 10:10:08 +0800

    202012
    改为多级配置

--- app/model/loadConfig.php
+++ app/model/loadConfig.php
@@ -7,7 +7,21 @@ class loadConfig {
      }
      static function get($key){
          $re=new self(); 
-         return self::$con[$key];
+         $key_arr=explode('.',$key);
+         if(count($key_arr)>1){  
+            $res=self::$con[$key_arr[0]];
+            for($i=1;$i<count($key_arr);$i++){ 
+                if(isset($res[$key_arr[$i]])){
+                    $res=$res[$key_arr[$i]];
+                }else{
+                    $res='';
+                    break;
+                } 
+            }
+            return $res;
+         }else{
+            return isset(self::$con[$key])?self::$con[$key]:'';
+         } 
      }
  
  }
